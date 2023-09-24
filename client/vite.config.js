fe1fadc8ca6f2bec4e24add73eae6d36277fd92e
einfachvalle <34622900+EinfachValle@users.noreply.github.com>
2023-09-25 00:22:56 +0200

    default setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    emptyOutDir: true,
+  },
+  server: {
+    port: 8080,
+    host: true,
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
